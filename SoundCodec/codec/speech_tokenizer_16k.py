bcabbe9ecb71b5d5d4c1b23e20a8fab723db5531
zhanghaitong01 <zhanghaitong01@corp.netease.com>
2024-11-01 17:16:38 +0800

    fix model download

--- SoundCodec/codec/speech_tokenizer_16k.py
+++ SoundCodec/codec/speech_tokenizer_16k.py
@@ -1,14 +1,19 @@
 from SoundCodec.base_codec.speech_tokenizer import BaseCodec
 import nlp2
+import os
 
 class Codec(BaseCodec):
     def config(self):
-        nlp2.download_file(
-            'https://huggingface.co/fnlp/SpeechTokenizer/raw/main/speechtokenizer_hubert_avg/config.json',
-            'speechtokenizer_hubert_avg')
-        self.config_path = "speechtokenizer_hubert_avg/config.json"
-
-        nlp2.download_file(
-            'https://huggingface.co/fnlp/SpeechTokenizer/resolve/main/speechtokenizer_hubert_avg/SpeechTokenizer.pt',
-            "speechtokenizer_hubert_avg")
-        self.ckpt_path = "speechtokenizer_hubert_avg/SpeechTokenizer.pt"
+        self.config_path = "SpeechTokenizer/speechtokenizer_hubert_avg/config.json"
+        # self.config_path = "speechtokenizer_hubert_avg/config.json"
+        if not os.path.exists(self.config_path):
+            nlp2.download_file(
+                'https://huggingface.co/fnlp/SpeechTokenizer/raw/main/speechtokenizer_hubert_avg/config.json',
+                'speechtokenizer_hubert_avg')
+        self.ckpt_path = "SpeechTokenizer/speechtokenizer_hubert_avg/SpeechTokenizer.pt"
+        # self.ckpt_path = "speechtokenizer_hubert_avg/SpeechTokenizer.pt"
+        if not os.path.exists(self.ckpt_path):
+            nlp2.download_file(
+                'https://huggingface.co/fnlp/SpeechTokenizer/resolve/main/speechtokenizer_hubert_avg/SpeechTokenizer.pt',
+                "speechtokenizer_hubert_avg")
+        
